69b759f9a52fac837f9b454700d154eb652ca4ac
maddy varner <maddy@badlandsunilimited.com>
2017-03-21 17:47:05 -0400

    added obligations format and wrote distractibility function

--- taboo.py
+++ taboo.py
@@ -4,21 +4,37 @@ from datetime import datetime, timedelta
 
 class Taboo():
     def __init__(self):
-        self.obligations = [] # {activity, cron reoccurance}
+        self.obligations = [
+            {
+              'activity': "nap", 
+              "start": (16, 0), 
+              "length": (3, 0), 
+              "frequency": [0,1,2,3,4,5,6], 
+              "distractibility": 80
+            },
+            {
+              "activity": "therapy", 
+              "start": (15, 0), 
+              "length": (1, 0), 
+              "frequency": [2], 
+              "distractibility": 0
+            }
+        ] 
         self.trust = 10 # / 100
 
-    def help(self, ):
-        frick
-
-    def obligated(self, datetime):
-        now  = datetime.datetime.now()
-
-        for obligation in self.obligations:
-            # [min, hour, day/month, month, day/week]
-            cron = obligation.cron.split(" ")
-            # check day of week
-            # check month
-            # check hour/minute
+    def distractibility(self, now):
+        # go through and check if there's an obligation right now
+        for obligation in obligations:
+            if now.weekday() in obligation['frequency']:
+                # get length
+                (l_hr, l_min) = obligation["length"]
+                length = timedelta(hours=l_hr, minutes=l_min)
+                # get start time
+                (s_hr, s_min) = obligation["start"]
+                start  = now.replace(hour=int(s_hr), minute=int(s_min), second=0, microsecond=0)
+                if now > start and now < (start + length):
+                    return obligation["distractibility"]
+        #if not she's free!
         return 100
 
     # questions
